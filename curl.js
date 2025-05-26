37530fc3d1633e00d5623c31b4344be0518cd84a
loveagri <282656050@qq.com>
2025-05-26 21:43:18 +0800

    msg

--- curl.js
+++ curl.js
@@ -1,7 +1,7 @@
 var myHeaders = new Headers()
 myHeaders.append('User-Agent', 'Apifox/1.0.0 (https://apifox.com)')
 myHeaders.append('Accept', '*/*')
-myHeaders.append('Host', '49.232.53.251')
+myHeaders.append('Host', 'test.dotohi.com')
 myHeaders.append('loveagri', 'you')
 myHeaders.append('Connection', 'keep-alive')
 
@@ -11,7 +11,7 @@ var requestOptions = {
 	redirect: 'follow',
 }
 
-fetch('http://49.232.53.251/', requestOptions)
+fetch('https://test.dotohi.com/', requestOptions)
 	.then((response) => response.text())
 	.then((result) => console.log(result))
 	.catch((error) => console.log('error', error))
